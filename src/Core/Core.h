06f438f73ba9a34ed6b53b8cde2b4b287817d35a
Zathyy <100758861+Zathyy@users.noreply.github.com>
2025-06-28 16:50:16 +0200

    initial project (cmake setup)

---
+++ src/Core/Core.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#ifdef RUI_SHARED
+  #ifdef _WIN32
+    #ifdef RUI_BUILD
+      #define RUI_API __declspec(dllexport)
+    #else
+      #define RUI_API __declspec(dllimport)
+    #endif
+  #else
+    #ifdef RUI_BUILD
+      #define RUI_API __attribute__((visibility("default")))
+    #else
+      #define RUI_API
+    #endif
+  #endif
+#else
+  #define RUI_API
+#endif
+
+
