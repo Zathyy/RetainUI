b1e3d79b18700275b20ca34908998ecef861a035
Daniel Nordlund <dannerune@hotmail.se>
2025-06-28 21:42:05 +0200

    Added point class and fixed gitignore

---
+++ src/Math/Point.h
@@ -0,0 +1,22 @@
+
+#include "Core/Core.h"
+
+namespace rui
+{
+	struct RUI_API Point
+	{
+		Point()
+		{
+			X = 0;
+			Y = 0;
+		}
+		Point(const Point& value)
+		{
+			X = value.X;
+			Y = value.Y;
+		}
+
+		int X;
+		int Y;
+	};
+}
